b2f7df6c06647d0ffa20db8e8a8fb6a77b343a4f
Lim Zhi Lit <g00440629@atu.ie>
2025-10-06 09:58:34 +0100

    Start project

---
+++ src/main/java/ie/atu/StudentApp.java
@@ -0,0 +1,51 @@
+package ie.atu;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class StudentApp {
+
+    public static void main(String[] args) {
+
+        ArrayList<Student> students = new ArrayList<>();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Insert number of students:");
+        int numberOfStudents = sc.nextInt();
+        sc.nextLine();
+
+        String name = "";
+        String email = "";
+        String course = "";
+        for (int i = 0; i <= numberOfStudents; i++) {
+            System.out.println("Insert name:");
+            name = sc.nextLine().trim();
+            System.out.println("Insert email:");
+            email = sc.nextLine().trim().toLowerCase();
+            if (isEmailDuplicate(students, email)) {
+                System.out.println("Email already exists. Cannot add student.");
+                continue;
+            }
+            System.out.println("Insert course:");
+            course = sc.nextLine().trim();
+            students.add(new Student(name, email, course));
+            System.out.println();
+        }
+
+        for (Student student : students) {
+            System.out.println(student);
+        }
+
+    }
+
+    public static boolean  isEmailDuplicate(ArrayList<Student> students, String email) {
+        for (Student s : students) {
+            if (s.getEmail().equalsIgnoreCase(email)) {
+                return true; // Duplicate found
+            }
+        }
+        return false; // No duplicate
+    }
+
+
+}
